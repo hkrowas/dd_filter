e3cca2fec8795d109431cc865a2ed91a665e2c79
Harrison Krowas <hkrowas@gmail.com>
2017-04-11 16:54:46 -0700

    Add FIR_FILTER entity.

--- fir_filter.vhd
+++ fir_filter.vhd
@@ -15,11 +15,33 @@
 --      data_in -  Input data to filter. New data should be given on every clock
 --      taps_reset     -  Taps take these values when reset
 --      taps_in        -  Taps take these values on every colck
---      reset          -  Sets tap values to taps input
+--      reset          -  Sets tap values to taps_reset
 --
 --  Outputs:
+--      taps         -  Value of taps
 --      data_out     -  Output data from filter.
---      error_out    -  Computed error signal. Used for testing purposes.
 --
 --  Generic:
 --      n_taps   -  Number of taps
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.numeric_std.all;
+
+use work.tap_array.all;
+
+entity FIR_FILTER is
+    generic (
+        n_taps  :  integer := 16
+    );
+    port (
+        clock      :  in  std_logic;
+        data_in    :  in  std_logic_vector(15 downto 0);
+        taps_reset :  in  tap_array(0 to n_taps - 1);
+        taps_in    :  in  tap_array(0 to n_taps - 1);
+        reset      :  in  std_logic;
+        taps       :  out tap_array(0 to n_taps - 1);
+        data_out   :  buffer std_logic_vector(15 downto 0)
+    );
+end FIR_FILTER;
