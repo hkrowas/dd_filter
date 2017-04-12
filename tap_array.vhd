bc34d10359e6487b4faa7e15d1f5e556bec2ab47
Harrison Krowas <hkrowas@gmail.com>
2017-04-11 17:00:56 -0700

    Actually add tap_array.vhd

---
+++ tap_array.vhd
@@ -0,0 +1,8 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.numeric_std.all;
+
+package tap_array is
+    type  tap_array is array(integer range <>) of std_logic_vector(15 downto 0);
+end tap_array;
