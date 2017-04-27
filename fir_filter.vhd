0a97063b8c84a62b76f8638983b8ea5ef385e4d8
Harrison Krowas <hkrowas@gmail.com>
2017-04-27 15:20:12 -0700

    Add fir_filter architecture. Has not been tested.

--- fir_filter.vhd
+++ fir_filter.vhd
@@ -26,8 +26,8 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
 use work.tap_array.all;
 
@@ -37,11 +37,62 @@ entity FIR_FILTER is
     );
     port (
         clock      :  in  std_logic;
-        data_in    :  in  std_logic_vector(15 downto 0);
+        data_in    :  in  com;
         taps_reset :  in  tap_array(0 to n_taps - 1);
         taps_in    :  in  tap_array(0 to n_taps - 1);
         reset      :  in  std_logic;
-        taps       :  out tap_array(0 to n_taps - 1);
-        data_out   :  buffer std_logic_vector(15 downto 0)
+        taps       :  buffer tap_array(0 to n_taps - 1);
+        data_out   :  buffer com
     );
 end FIR_FILTER;
+
+architecture FIR_FILTER_ARCH of FIR_FILTER is
+    signal ein : tap_array(n_taps - 1 downto 0);
+    signal products : tap_array(n_taps - 1 downto 0);
+    signal sums : tap_array(n_taps - 1 downto 0);
+    component COM_MUL_ARRAY
+        generic (
+            n  :  integer := 16
+        );
+        port (
+            z  :  in  com;
+            w  :  in  com;
+            c  :  out com
+        );
+    end component;
+begin
+    -- Multiplier array generate
+    MUL_TAP_ARRAY : for i in 0 to n_taps - 1 generate
+    begin
+        MULS : COM_MUL_ARRAY
+        port map (
+            z => ein(i),
+            w => taps(i),
+            c => products(i)
+        );
+    end generate;
+    sums(0)(0) <= products(0)(0);
+    sums(0)(1) <= products(0)(1);
+    SUM_ARRAY : for i in 1 to n_taps - 1 generate
+    begin
+        sums(i)(0) <= sums(i - 1)(0) + products(i)(0);
+        sums(i)(1) <= sums(i - 1)(1) + products(i)(1);
+    end generate;
+    data_out <= sums(n_taps - 1);
+    process (clock)
+    begin
+        if (rising_edge(clock)) then
+            -- Delay line
+            ein(0) <= data_in;
+            for i in 1 to n_taps - 1 loop
+                ein(i) <= ein(i - 1);
+            end loop;
+            -- Taps update
+            if (reset = '1') then
+                taps <= taps_reset;
+            else
+                taps <= taps_in;
+            end if;
+        end if;
+    end process;
+end FIR_FILTER_ARCH;
--- tap_array.vhd
+++ tap_array.vhd
@@ -4,6 +4,6 @@ use ieee.std_logic_arith.all;
 use ieee.numeric_std.all;
 
 package tap_array is
-    type  tap_array is array(integer range <>) of std_logic_vector(15 downto 0);
     type  com is array(0 to 1) of std_logic_vector(15 downto 0);
+    type  tap_array is array(integer range <>) of com;
 end tap_array;
