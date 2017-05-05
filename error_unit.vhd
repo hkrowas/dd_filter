f1efbbb5c46d5174ead0d174c736c5c4d97ce2bc
Harrison Krowas <hkrowas@gmail.com>
2017-05-05 00:42:46 -0700

    Add architecture for ERROR_UNIT (error_unit.vhd).

--- error_unit.vhd
+++ error_unit.vhd
@@ -28,8 +28,8 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
 use work.tap_array.all;
 
@@ -38,8 +38,9 @@ entity ERROR_UNIT is
         n_taps  :  integer := 16
     );
     port (
-        data_in    :  in  std_logic_vector(15 downto 0);
-        data_out   :  in  std_logic_vector(15 downto 0);
+        data_in    :  in  tap_array(0 to n_taps - 1);
+        data_out   :  in  com;
+        taps_in    :  in  tap_array(0 to n_taps - 1);
         taps       :  out tap_array(0 to n_taps - 1);
         taps_error :  out tap_array(0 to n_taps - 1);
         error_out  :  out std_logic_vector(15 downto 0)
@@ -57,6 +58,60 @@ architecture ERROR_UNIT_ARCH of ERROR_UNIT is
             c  :  out com
         );
     end component;
+    component MUL_ARRAY
+        generic (
+            n  :  integer := 16
+        );
+        port (
+            x  :  in  std_logic_vector(n - 1 downto 0);
+            y  :  in  std_logic_vector(n - 1 downto 0);
+            z  :  out std_logic_vector(2 * n - 1 downto 0)
+        );
+    end component;
+    signal const_i : std_logic_vector(3 downto 0);
+    signal bot : std_logic_vector(15 downto 0);
+    signal left : std_logic_vector(15 downto 0);
+    signal e : com;
+    signal mu : std_logic_vector(15 downto 0) := x"0010";
+    signal mu_e : com;
+    signal mu_e_in : tap_array(0 to n_taps - 1);
+    signal data_in_mul : tap_array(0 to n_taps - 1);
 begin
-
+    -- Use sign of real and imaginary parts of data_out to determine quadrant.
+    const_i(3) <= not(data_out(0)(15));
+    const_i(1) <= not(data_out(1)(15));
+    -- Find whether data_out is in bottom and left of the quadrant.
+    bot <= ('0' & data_out(1)(14 downto 0)) - p2_p1_d_2;
+    left <= ('0' & data_out(0)(14 downto 0)) - p2_p1_d_2;
+    const_i(2) <= left(15);
+    const_i(0) <= bot(15);
+    e(0) <= QAM16(to_integer(unsigned(const_i)))(0) - data_out(0);
+    e(1) <= QAM16(to_integer(unsigned(const_i)))(1) - data_out(1);
+    -- Calculate error
+    E_R_MUL : MUL_ARRAY
+    port map (
+        x => e(0),
+        y => mu,
+        z(30 downto 15) => mu_e(0)
+    );
+    E_I_MUL : MUL_ARRAY
+    port map (
+        x => e(1),
+        y => mu,
+        z(30 downto 15) => mu_e(1)
+    );
+    MU_E_IN_MUL_generate : for i in 0 to n_taps - 1 generate
+        data_in_mul(i)(0) <= data_in(i)(0);
+        data_in_mul(i)(1) <= not(data_in(i)(1)) + 1;
+        MU_E_IN_MUL : COM_MUL_ARRAY
+        port map (
+            z => mu_e,
+            w => data_in_mul(i),
+            c => mu_e_in(i)
+        );
+    end generate;
+    NEW_TAPS: for i in 0 to n_taps - 1 generate
+        taps(i)(0) <= taps_in(i)(0) + mu_e_in(i)(0);
+        taps(i)(1) <= taps_in(i)(1) + mu_e_in(i)(1);
+    end generate;
 end ERROR_UNIT_ARCH;
--- tap_array.vhd
+++ tap_array.vhd
@@ -1,15 +1,26 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
 package tap_array is
     type  com is array(0 to 1) of std_logic_vector(15 downto 0);
     type  tap_array is array(integer range <>) of com;
     type  constellation_array is array(0 to 15) of com;
-    constant QAM16 : constellation  :=
-        ((x"C000", x"4000"), (x"A000", x"4000"), (x"2000", x"4000"), (x"4000", x"4000"),
-         (x"C000", x"2000"), (x"A000", x"2000"), (x"2000", x"2000"), (x"4000", x"2000"),
-         (x"C000", x"A000"), (x"A000", x"A000"), (x"2000", x"A000"), (x"4000", x"A000"),
-         (x"C000", x"C000"), (x"A000", x"C000"), (x"2000", x"C000"), (x"4000", x"C000"));
+    constant p1  :  std_logic_vector(15 downto 0) := x"2000";
+    constant p2  :  std_logic_vector(15 downto 0) := x"4000";
+    constant p2_p1 : std_logic_vector(15 downto 0) := p2 - p1;
+    constant p2_p1_d_2 : std_logic_vector(15 downto 0) := '0' & p2_p1(15 downto 1);
+    constant m1  :  std_logic_vector(15 downto 0) := '1' & p1(14 downto 0);
+    constant m2  :  std_logic_vector(15 downto 0) := '1' & p2(14 downto 0);
+    -- 16QAM constellation used:
+    --  2  6  14  10
+    --  3  7  15  11
+    --  1  5  13  9
+    --  0  4  12  8
+    constant QAM16 : constellation_array  :=
+        ((m2, m2), (m2, m1), (m2, p2), (m2, p1)
+       , (m1, m2), (m1, m1), (m1, p2), (m1, p1)
+       , (p2, m2), (p2, m1), (p2, p2), (p2, p1)
+       , (p1, m2), (p1, m1), (p1, p2), (p1, p1));
 end tap_array;
