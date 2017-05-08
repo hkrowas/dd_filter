Fix sign error in plot_output.py. Add ein to filter output.

generate_input.py
@@ -16,8 +16,11 @@ def int_to_bin(datain):
 N = 10000
 p2 = 0x3000
 p1 = 0x1000
-m2 = 0xB000
-m1 = 0x9000
+m2 = -p1
+m1 = -p2
+
+print(m2)
+print(m1)
 
 constellation = np.array([complex(m2, p2), complex(m1, p2), complex (p1, p2),
 complex(p2, p2), complex(m2, p1), complex(m1, p1), complex (p1, p1),
@@ -33,5 +36,8 @@ f = open('input', 'w')
 for i in range(N):
     f.write(int_to_bin(int(datain[i].real)) + " ")
     f.write(int_to_bin(int(datain[i].imag)) + " ")
+
+f.write("\n")
+f.write("\n")
 
 f.close()